3a3966143e882df112b6838136d87a771bbfb4c6
Jimmy Hong <jimmyhong.rocks@gmail.com>
2017-10-28 17:16:19 -0500

    hover user track list by button click

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,19 +1,38 @@
 (function() {
-	console.log('Home page!');
 	var turbo = Turbo({ site_id: '59f257cb8a9c7500124d6626' });
+	var stations = {};
+	var selectedStation = null;
+
+	var selectStation = function(event) {
+		event.preventDefault();
+		selectedStation = stations[event.target.id];
+        console.log('Select Station: ' + selectedStation);
+        
+        $('#selected-station-name').html(selectedStation.name);
+        var trackList = '';
+        if(selectedStation.tracks != null) {
+            selectedStation.tracks.forEach(function(track, i) {
+                trackList += '<li class="nav-item">';
+                trackList += ' <a class="nav-link" href="#">'+ track.name+'</a>';
+                trackList += '</li>';
+            });
+
+        }
+        $('#selected-station-tracks').html(trackList);
+		$('#button-toggle').click();
+	};
 
 	turbo.fetch('user', null, function(err, data) {
 		if (err) {
 			console.log('Error: ' + err);
 			return;
 		}
-		console.log('Users: ' + JSON.stringify(data));
 
 		var users = data.results;
 		var stationsHtml = '';
 
 		users.forEach(function(user, i) {
-            var numTracks = (user.tracks) ? user.tracks.length : 0;
+			var numTracks = user.tracks ? user.tracks.length : 0;
 			stationsHtml += '<div class="col-12 col-md-4"><div class="pricing-1">';
 			stationsHtml += '<p class="plan-name">' + user.name + '</p><br>';
 			stationsHtml +=
@@ -22,7 +41,16 @@
 			stationsHtml += '<small>Basic support</small><br>';
 			stationsHtml += '<small>Sync to cloud database</small><br> <br>';
 			stationsHtml +=
-				'<p class="text-center py-3"><a class="btn btn-primary" href="#">View Tracks</a></p> </div></div>';
+				'<p class="text-center py-3"><a id="' +
+				user.id +
+				'" class="btn btn-primary" href="#">View Tracks</a></p> </div></div>';
+			stations[user.id] = user;
+
+			setTimeout(function() {
+				$('#' + user.id).click(selectStation);
+			}, 500);
+
+			// setTimeout(selectStation, 500);
 		});
 
 		$('#stations').html(stationsHtml);
